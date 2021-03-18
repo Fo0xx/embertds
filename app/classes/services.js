02726085c68636f71e01a1325e0022231fac4750
V_Huard <21902424@etu.unicaen.fr>
2021-03-18 14:47:28 +0100

    TD2

---
+++ app/classes/services.js
@@ -0,0 +1,31 @@
+import { computed } from '@ember/object';
+
+export default class Services {
+
+  services = [];
+
+  constructor(serveices) {
+    this.services = serveices;
+  }
+
+  @computed('services.@each.active')
+  get countActive(){
+    /*let r=0;
+    this.services.forEach(service=>{
+      if(service.active)
+        r++;
+    });
+    return r;*/
+    return this.services.filterBy('active', true).length;
+  }
+
+  get sumActive(){
+    let services = this.services.filterBy('active', true);
+    let price = 0;
+    services.forEach(service=>{
+      price += service.price;
+    });
+    return price;
+  }
+
+}
--- app/routes/ex1b.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-import { action } from '@ember/object';
-
-export default class Ex1bRoute extends Route {
-  model(){
-    return this.modelFor('ex1').items;
-  }
-}
