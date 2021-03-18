f0c5eec0077d5aae7fe056cb3a327dbe8436a180
V_Huard <21902424@etu.unicaen.fr>
2021-03-18 15:28:32 +0100

    TD4

--- app/helpers/array-contains.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function arrayContains(params/*, hash*/) {
-  const [items, value,prop] = params;
-  let id=prop ||'id'
-  return items.filterBy(id,value).length > 0;
-});
--- tests/unit/routes/test-new-test.js
+++ tests/unit/routes/test-new-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | step1', function(hooks) {
+module('Unit | Route | testNew', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:step1');
+    let route = this.owner.lookup('route:test-new');
     assert.ok(route);
   });
 });
